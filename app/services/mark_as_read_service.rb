196c785f2da74276753b8c84eea5b5bb8265f427
tolves <tov.sharing@gmail.com>
2020-09-22 05:43:26 +0100

    optimise the logic and style

--- app/services/mark_as_read_service.rb
+++ app/services/mark_as_read_service.rb
@@ -4,9 +4,12 @@ class MarkAsReadService
   end
 
   def call
-    Message.unread.where('inbox_id = ? AND outbox_id = ?', @message.inbox_id, @message.outbox_id).update(read: true)
+    set_messages_read
   end
 
   private
 
+  def set_messages_read
+    Message.unread.where('inbox_id = ? AND outbox_id = ?', @message.inbox_id, @message.outbox_id).update(read: true)
+  end
 end
